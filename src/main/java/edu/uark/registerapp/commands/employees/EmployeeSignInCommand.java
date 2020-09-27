0997d7084df1bfdd7a1e5e0e54657c631bafc11c
ChronicEel93 <wktradewell@gmail.com>
2020-09-27 16:11:37 -0500

    Typo

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -0,0 +1,10 @@
+package edu.uark.registerapp.commands.employees;
+
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+
+@Service
+public class EmployeeSignInCommand implements VoidCommandInterface {
+    
+}

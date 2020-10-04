c6385ba3e9e9349d3ef5ddedc825d09c5bb9f3d0
Keaton Hurlbut <keatonhurlbut@gmail.com>
2020-10-04 13:21:36 -0500

    Trying to push changes to signIn.js for task 3

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -4,5 +4,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function validateForm() {
 	// TODO: Validate the user input
+	id = document.getElementById('employee_id').nodeValue;
+	pw = document.getElementById('password').nodeValue;
+	if (!id) // if id is empty
+		return false;
+	if (!pw) // if password is empty
+		return false;
+	if (Number(id) != id) // if id has something besides numbers in it
+		return false;
 	return true;
 }

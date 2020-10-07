f7a8084744e11ee8419b835afd40c4226e50c847
Keaton Hurlbut <keatonhurlbut@gmail.com>
2020-10-07 09:53:16 -0500

    Task 7. First part (mainMenu.js) ready for testing, second part (master.js) not

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -2,7 +2,40 @@ let hideEmployeeSavedAlertTimer = undefined;
 
 document.addEventListener("DOMContentLoaded", () => {
 	// TODO: Things that need doing when the view is loaded
+
+	/*
+	Task 10
+	• On page load[1], define a “click” event handler[2] for the “Save” button defined in the view
+		◦ Functionality should validate the input before proceeding
+	▪ First name should not be blank
+	▪ Last name should not be blank
+	▪ Password should not be blank and should equal the value of the Confirm Password input
+	▪ Employee type should be “Cashier”, “Shift Manager”, or “General Manager”
+		◦ If validation fails
+	▪ Display an appropriate error
+	▪ Focus and select the offending element
+	▪ Interrupt/stop the save functionality
+		◦ Perform an HTTP request, via AJAX[3], to the server to save the employee details
+	▪ Perform a POST request[4,5] if the employee is new
+	▪ Perform a PATCH request[4,5] if the employee exists in the database
+		◦ After saving the employee and if the employee ID input element is not visible then display
+		the employee ID input element
+	▪ Input should remain disabled[6]
+
+	*/
+	document.getElementById('save').addEventListener('click', function() {
+		var firstName = document.getElementById('firstName').value;
+		var lastName = document.getElementById('lastName').value;
+		var pw = document.getElementById('password').value;
+
+		//if (firstName == '')
+
+
+	});
+
 });
+
+//function invalidResponse(invalidField, )
 
 // Save
 function saveActionClick(event) {

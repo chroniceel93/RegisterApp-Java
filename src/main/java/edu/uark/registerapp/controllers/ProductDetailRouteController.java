4c21f520ac5bc1399d00a07b3fbccaf77e02338f
Yaling <linger2010@hotmail.com>
2020-10-09 14:20:16 -0500

    Added not-elevated-user features to ProductDetailRouteController

--- src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
@@ -1,6 +1,10 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Optional;
 import java.util.UUID;
+
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +18,42 @@ import edu.uark.registerapp.commands.products.ProductQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
 @RequestMapping(value = "/productDetail")
-public class ProductDetailRouteController {
+public class ProductDetailRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start() {
-		return (new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()))
-			.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+	public ModelAndView start(final HttpServletRequest request) {
+		Optional<ActiveUserEntity> activeUser = this.getCurrentUser(request);
+		if(activeUser.isPresent()) {
+			if(this.isElevatedUser(activeUser.get())) {
+				return (new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()))
+				.addObject(
+					ViewModelNames.PRODUCT.getValue(),
+					(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0))
+				.addObject(ViewModelNames.IS_ELEVATED_USER.getValue(),true);
+			} else {
+				return this.buildNoPermissionsResponse(ViewNames.PRODUCT_LISTING.getRoute());
+			}
+		} else {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.SIGN_IN.getRoute()));
+		}
+
 	}
 
 	@RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-	public ModelAndView startWithProduct(@PathVariable final UUID productId) {
+	public ModelAndView startWithProduct(@PathVariable final UUID productId,final HttpServletRequest request) {
+		Optional<ActiveUserEntity> activeUser = this.getCurrentUser(request);
+		if(!activeUser.isPresent()) {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.SIGN_IN.getRoute()));
+		} // Else, activeUser exists, doNothing();
+
+
 		final ModelAndView modelAndView =
 			new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
 
@@ -35,6 +61,9 @@ public class ProductDetailRouteController {
 			modelAndView.addObject(
 				ViewModelNames.PRODUCT.getValue(),
 				this.productQuery.setProductId(productId).execute());
+			modelAndView.addObject(
+				ViewModelNames.IS_ELEVATED_USER.getValue(),
+				this.isElevatedUser(activeUser.get()));
 		} catch (final Exception e) {
 			modelAndView.addObject(
 				ViewModelNames.ERROR_MESSAGE.getValue(),
@@ -47,7 +76,9 @@ public class ProductDetailRouteController {
 		}
 
 		return modelAndView;
+
 	}
+
 
 	// Properties
 	@Autowired

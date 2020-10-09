60f565f11cd99591e2277efad7bfbfcacae9131f
Yaling <linger2010@hotmail.com>
2020-10-09 16:05:17 -0500

    Added not-elevated-user features to ProductListingRouteController

--- src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
@@ -1,5 +1,9 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +14,14 @@ import edu.uark.registerapp.commands.products.ProductsQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
 @RequestMapping(value = "/productListing")
-public class ProductListingRouteController {
+public class ProductListingRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView showProductListing() {
+	public ModelAndView showProductListing(final HttpServletRequest request) {
+		Optional<ActiveUserEntity> activeUser = this.getCurrentUser(request);
 		ModelAndView modelAndView =
 			new ModelAndView(ViewNames.PRODUCT_LISTING.getViewName());
 
